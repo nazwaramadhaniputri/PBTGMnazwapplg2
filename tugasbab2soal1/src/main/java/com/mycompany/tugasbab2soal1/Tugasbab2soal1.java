5c1b1f72c2e643758bbaef38606fe1f2c379eb64
nazwaramadhaniputri <145530562+nazwaramadhaniputri@users.noreply.github.com>
2024-08-12 15:18:51 +0700

    Add files via upload

---
+++ tugasbab2soal1/src/main/java/com/mycompany/tugasbab2soal1/Tugasbab2soal1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasbab2soal1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugasbab2soal1 {
+    
+
+    public static void main(String[] args) {
+       int a=50;
+       float b=a;
+       
+       System.out.println(a);
+       System.out.println(b);
+    }
+}
