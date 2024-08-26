d4d3c03b99ada541a67e0391d6fc33ab84b3a702
nazwaramadhaniputri <145530562+nazwaramadhaniputri@users.noreply.github.com>
2024-08-26 15:37:12 +0700

    Add files via upload

---
+++ tugasbab2soal2/src/main/java/com/mycompany/tugasbab2soal2/Tugasbab2soal2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasbab2soal2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugasbab2soal2 {
+public static String Makanan;
+//Membuat variable tanpa Value/Nilai pada class staticvariable
+ public static final String Minuman = "jus jambu";
+    public static void main(String[] args) {
+ // TODO code application logic here
+ Makanan = "ayam geprek ";//Mengisi Value pada variable Hobi
+ System.out.println("Favorit Saya " + Makanan);//Memanggil Variable Hobi
+ System.out.println("Favorit Saya " + Minuman);//Memanggil Variable Hewan
+ } 
+}
---
+++ tugasbab3soal1/src/main/java/com/mycompany/tugasbab3soal1/Tugasbab3soal1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasbab3soal1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugasbab3soal1 {
+
+    public static void main(String[] args) {
+        byte a = 9;
+        a = 9;
+        System.out.println(9);
+        float fl = 8.99f;
+        System.out.println(8.99f);
+        char A = 'A';
+        System.out.println(A);
+        boolean b = false;
+        System.out.println(b);
+        System.out.println("hello world");
+    }
+}
