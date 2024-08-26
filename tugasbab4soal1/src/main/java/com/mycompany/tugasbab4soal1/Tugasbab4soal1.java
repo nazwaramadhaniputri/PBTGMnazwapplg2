116a7d404c9e5a37d7694a0f1f27164feefaf182
nazwaramadhaniputri <145530562+nazwaramadhaniputri@users.noreply.github.com>
2024-08-26 16:03:40 +0700

    Add files via upload

---
+++ tugasbab4soal1/src/main/java/com/mycompany/tugasbab4soal1/Tugasbab4soal1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasbab4soal1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugasbab4soal1 {
+
+    public static void main(String[] args) {
+    int angka1 = 10;
+ int angka2 = 3;
+ double pembagian = (double)angka1 / angka2;
+System.out.println("Hasil pertambahan a+b =  " +
+(angka1 + angka2) );
+System.out.println("Hasil perkurangan a-b = " +
+(angka1 - angka2) );
+System.out.println("Hasil perkalian a*b = " +
+(angka1 * angka2) );
+System.out.println("Hasil perkalian a/b =  " +
+pembagian);
+    }
+}
