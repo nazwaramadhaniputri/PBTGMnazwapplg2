02e02a5a3a5690bced26237be8f0f4c592c11812
nazwaramadhaniputri <145530562+nazwaramadhaniputri@users.noreply.github.com>
2024-08-28 08:13:26 +0700

    Add files via upload

---
+++ tugasbab7soal1/src/main/java/com/mycompany/tugasbab7soal1/Tugasbab7soal1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasbab7soal1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugasbab7soal1 {
+
+    public static void main(String[] args) {
+        int day = 2;
+        switch ( day ) {
+          case 1:
+            System.out.println("Sabtu ");
+            break;
+          case 2:
+            System.out.println("Minggu ");
+            break;
+        }
+    }
+}
